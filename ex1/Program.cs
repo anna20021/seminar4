ebeadf5d12c14e6c1e565308032d40c1dadbbaf8
Снежана Павалаки <snezanapavalaki@MacBook-Air-Snezana.local>
2023-09-07 11:00:22 +0300

    added ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+Console.WriteLine("enter numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+
+for (int i = 1; i <= numberB; i= i+1);
+
+{
+  c = c * numberA * numberA;
+}
+Console.WriteLine ( c );
